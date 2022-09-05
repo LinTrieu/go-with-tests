e10d305ff1a21ab234a0c1cfcbb7dedef60c0440
LinTrieu <linnatrieu@gmail.com>
2022-09-04 18:16:11 -0700

    sum all

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -11,5 +12,14 @@ func TestSum(t *testing.T) {
 
 	if expected != got {
 		t.Errorf("Expected: %v, Got: %v, Given: %v", expected, got, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
