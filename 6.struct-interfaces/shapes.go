8a889c42b1d1b9a055d7597be0da2d24db1f2ee3
LinTrieu <linnatrieu@gmail.com>
2022-09-15 17:03:11 -0700

    adds circle struct

--- 6.struct-interfaces/shapes.go
+++ 6.struct-interfaces/shapes.go
@@ -2,15 +2,27 @@
 // https: //quii.gitbook.io/learn-go-with-tests/go-fundamentals/structs-methods-and-interfaces
 package main
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
