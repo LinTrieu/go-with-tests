bb4af8235aa4a38f214437f7f9f58659bbd864ef
LinTrieu <linnatrieu@gmail.com>
2022-09-26 13:01:36 -0700

    adds TestSearch

--- 8.maps/dictionary_test.go
+++ 8.maps/dictionary_test.go
@@ -1,3 +1,19 @@
 // Maps
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps
+// store items by a key + look them up quickly
 package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{
+		"test": "this is a test",
+	}
+
+	got := Search(dictionary, "test")
+	want := "this is a test"
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
