e2d3158668ad9f2fc56bccb742048cc685bf7e8b
LinTrieu <linnatrieu@gmail.com>
2022-09-26 14:35:32 -0700

    fixes update

--- 8.maps/dictionary.go
+++ 8.maps/dictionary.go
@@ -27,3 +27,17 @@ func (d Dictionary) Add(word string, definition string) error {
 
 	return nil
 }
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
--- 8.maps/errors.go
+++ 8.maps/errors.go
@@ -1,8 +1,9 @@
 package main
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
