49d24c55fd27e36baf1a3a5b8b0ddc64d4e5a0b0
LinTrieu <linnatrieu@gmail.com>
2022-09-26 13:27:15 -0700

    fixes test

--- 8.maps/dictionary.go
+++ 8.maps/dictionary.go
@@ -1,3 +1,9 @@
 // Maps
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps
 package main
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
