e86251b62d0c93b0f0fca8e9bbda5ccbf9af70b6
LinTrieu <linnatrieu@gmail.com>
2022-11-17 16:40:36 -0800

    adds test for dependency injection

--- 9.dependency-injection/di.go
+++ 9.dependency-injection/di.go
@@ -1,3 +1,13 @@
 // Dependency Injection
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/dependency-injection
+// There is an article by Martin Fowler: https://martinfowler.com/articles/injection.html
 package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Printf("Hello, %s", name)
+}
--- 9.dependency-injection/di_test.go
+++ 9.dependency-injection/di_test.go
@@ -1,3 +1,25 @@
 // Dependency Injection
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/dependency-injection
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// we need inject the dependency of printing.
+// Our function doesn't care where or how the printing happens
+// so we should accept an interface rather than a concrete type.
+
+// The Buffer type implements the Writer interface, because it has the method Write(p []byte) (n int, err error).
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Evelyn")
+
+	got := buffer.String()
+	want := "Hello, Evelyn"
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
