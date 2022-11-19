c44e606bb9fd469a6e9f737e936a009187812b67
LinTrieu <linnatrieu@gmail.com>
2022-11-18 18:12:26 -0800

    running server and wwriting data using io Writer

--- 9.dependency-injection/di.go
+++ 9.dependency-injection/di.go
@@ -4,10 +4,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
-	fmt.Printf("Hello, %s", name)
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
+
+// fmt.Fprintf allows you to pass in an io.Writer which both os.Stdout and bytes.Buffer implement
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "worlds!")
 }
