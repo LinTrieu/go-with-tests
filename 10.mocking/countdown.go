5569af8f780c231c5ee94ab6823bf248ee1bcf44
LinTrieu <linnatrieu@gmail.com>
2022-11-18 19:25:47 -0800

    print to std output

--- 10.mocking/countdown.go
+++ 10.mocking/countdown.go
@@ -1,3 +1,17 @@
 // Mocking
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	Countdown(os.Stdout)
+}
+
+func Countdown(out io.Writer) {
+	fmt.Fprint(out, "3")
+}
--- 10.mocking/countdown_test.go
+++ 10.mocking/countdown_test.go
@@ -2,3 +2,24 @@
 // https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/mocking
 
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// Our software needs to print to stdout
+// The bytes.Buffer type implements the Writer interface
+// So we'll use it in our test to send in as our Writer and then we can check what was written to it after we invoke Countrdown
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+
+	Countdown(buffer)
+
+	got := buffer.String()
+	want := "3"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
